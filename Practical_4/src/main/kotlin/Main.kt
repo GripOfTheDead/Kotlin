d2347e1d03f9fd0bf25660d834761837ce76cf50
GripOfTheDead <112634335+GripOfTheDead@users.noreply.github.com>
2022-10-11 12:06:54 +0300

    Add files via upload

---
+++ Practical_4/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    // Задание 1
+    fun printFullName() {
+        val firstName = "Konstantin"
+        val lastName = "Filip'ev"
+        println("$firstName $lastName")
+    }
+    // Задание 2
+    printFullName()
+    // Задание 3
+    fun calculateFullName()
+    {
+        val neme =
+    }
+}
